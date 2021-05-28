d2db6fcabefd8755981c8001f9dd9d9758680d12
pooria83 <31447843+pooria83@users.noreply.github.com>
2021-05-28 21:57:31 +0430

    Add product routes

--- productService/routes/web.php
+++ productService/routes/web.php
@@ -14,12 +14,13 @@
 */
 
 $router->group(['prefix' => 'v1/product', 'namespace' => 'v1'], function () use ($router) {
-    $router->get('/public_link/{id}', 'ProductControl@link');
     $router->group(['middleware' => 'token_check'], function () use ($router) {
+        $router->get('/public_link', 'ProductController@public_link');
         $router->group(['middleware' => 'admin_check'], function () use ($router) {
             $router->post('/submit', 'ProductController@store');
-            $router->post('/get_list', 'ProductController@list');
-            $router->post('/check_view', 'Product_UserController@check_view_count');
+            $router->get('/get_list', 'ProductController@list');
+            $router->post('/single' , 'ProductController@single');
+            $router->get('/check_view', 'Product_UserController@check_view_count');
             $router->group(['middleware' => 'update_and_delete_check'], function () use ($router) {
                 $router->post('/edit', 'ProductController@edit');
                 $router->post('/delete', 'ProductController@delete');
